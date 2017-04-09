4fb53313cb7998941e1bea19a1445731b0a35353
Christoph Heindl <christoph.heindl@gmail.com>
2017-04-09 12:43:07 +0200

    updated example

--- motmetrics/apps/example.py
+++ motmetrics/apps/example.py
@@ -17,28 +17,30 @@ if __name__ == '__main__':
         [[0.1, np.nan, 0.3],        # Distances from object 'a' to hypotheses 1, 2, 3
          [0.5,  0.2,   0.3]]        # Distances from object 'b' to hypotheses 1, 2, 
     )
+    print(acc.events)
 
     # 1 Match, 1 Miss
-    acc.update(
+    df = acc.update(
         ['a', 'b'],
         [1],
         [[0.2], [0.4]]
     )
+    print(df)
 
     # 1 Match, 1 Switch
-    acc.update(
+    df = acc.update(
         ['a', 'b'],
         [1, 3],
         [[0.6, 0.2],
          [0.1, 0.6]]
     )
+    print(df)
     
-    #print(acc.events)
+    summary = mm.metrics.summarize(acc)
+    print(mm.io.render_summary(summary))
 
-    print('Summary')
-    x = acc.events.loc[0:1]
-    summary = mm.metrics.summarize(x)
-    #print(mm.io.render_summary(summary))
+    summaries = mm.metrics.summarize([acc, acc.events.loc[0:1]], names=['full', 'part'])
+    print(mm.io.render_summary(summaries))
 
     
 
