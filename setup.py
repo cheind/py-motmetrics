7baa2f73d13c7470366285592f0a9872fa63c699
Christoph Heindl <christoph.heindl@gmail.com>
2017-04-09 14:09:21 +0200

    changed description

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 setup(
     name='motmetrics',
     version=open('motmetrics/__init__.py').readlines()[-1].split()[-1].strip('\''),
-    description='Metrics for multiple object tracker evaluation.',
+    description='Metrics for multiple object tracker benchmarking.',
     author='Christoph Heindl',
     url='https://github.com/cheind/py-motmetrics',
     packages=['motmetrics', 'motmetrics.test', 'motmetrics.apps'],
