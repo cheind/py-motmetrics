4ef661d97a5d44cdbbddd0306d91fd49c9c9a391
Christoph Heindl <christoph.heindl@gmail.com>
2017-04-10 12:40:45 +0200

    added app to batch-evaluate on MOTChallenge data

---
+++ motmetrics/apps/motchallenge.py
@@ -0,0 +1,87 @@
+"""py-motmetrics - metrics for multiple object tracker (MOT) benchmarking.
+
+Christoph Heindl, 2017
+https://github.com/cheind/py-motmetrics
+
+
+
+"""
+
+import argparse
+import glob
+import os
+import logging
+import motmetrics as mm
+from pathlib import Path
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="""
+Compute  metrics for trackers using MOTChallenge ground-truth data.
+
+Files
+-----
+All file content, ground truth and test files, have to comply with the
+format described in 
+
+Milan, Anton, et al. 
+"Mot16: A benchmark for multi-object tracking." 
+arXiv preprint arXiv:1603.00831 (2016).
+https://motchallenge.net/
+
+Structure
+---------
+
+Layout for ground truth data
+    <GT_ROOT>/<SEQUENCE_1>/gt/gt.txt
+    <GT_ROOT>/<SEQUENCE_2>/gt/gt.txt
+    ...
+
+Layout for test data
+    <TEST_ROOT>/<SEQUENCE_1>.txt
+    <TEST_ROOT>/<SEQUENCE_2>.txt
+    ...
+
+Sequences of ground truth and test will be matched according to the `<SEQUENCE_X>`
+string.""", formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('groundtruths', type=str, help='Directory containing ground truth files.')   
+    parser.add_argument('tests', type=str, help='Directory containing tracker result files')
+    parser.add_argument('--loglevel', type=str, help='Log level', default='info')
+    parser.add_argument('--fmt', type=str, help='Data format', default='mot15-2D')
+    return parser.parse_args()
+
+def evaluate_dataframes(gt, ts):
+    
+    accs = []
+    names = []
+    for k, tsacc in ts.items():
+        if k in gt:            
+            logging.info('Comparing {}...'.format(k))
+            accs.append(mm.utils.compare_to_groundtruth(gt[k], tsacc, 'iou', distth=0.5))
+            names.append(k)
+        else:
+            logging.warning('No ground truth for {}, skipping.'.format(k))
+
+    return accs, names
+
+if __name__ == '__main__':
+
+    args = parse_args()
+
+    loglevel = getattr(logging, args.loglevel.upper(), None)
+    if not isinstance(loglevel, int):
+        raise ValueError('Invalid log level: {} '.format(args.loglevel))        
+    logging.basicConfig(level=loglevel, format='%(asctime)s %(levelname)s - %(message)s', datefmt='%I:%M:%S')
+
+    gtfiles = glob.glob(os.path.join(args.groundtruths, '*/gt/gt.txt'))
+    tsfiles = [f for f in glob.glob(os.path.join(args.tests, '*.txt')) if not os.path.basename(f).startswith('eval')]
+
+    logging.info('Found {} groundtruths and {} test files.'.format(len(gtfiles), len(tsfiles)))
+    logging.info('Loading files.')
+    
+    gt = dict([(Path(f).parts[-3], mm.io.loadtxt(f, fmt='mot15-2D')) for f in gtfiles])
+    ts = dict([(os.path.splitext(Path(f).parts[-1])[0], mm.io.loadtxt(f, fmt='mot15-2D')) for f in tsfiles])
+    
+    accs, names = evaluate_dataframes(gt, ts)
+    summary = mm.metrics.summarize(accs, names) 
+    print(mm.io.render_summary(summary))
