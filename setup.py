1f38237204eb471360e59b63578e01f885ee92a3
Christoph Heindl <christoph.heindl@gmail.com>
2017-04-11 07:01:39 +0200

    adding requirements

--- setup.py
+++ setup.py
@@ -4,13 +4,19 @@ Christoph Heindl, 2017
 https://github.com/cheind/py-motmetrics
 """
 
+import os
 from distutils.core import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='motmetrics',
     version=open('motmetrics/__init__.py').readlines()[-1].split()[-1].strip('\''),
-    description='Metrics for multiple object tracker benchmarking.',
+    description='Metrics for multiple object tracker benchmarking.',    
     author='Christoph Heindl',
     url='https://github.com/cheind/py-motmetrics',
+    license='MIT',
+    install_requires=required,
     packages=['motmetrics', 'motmetrics.tests', 'motmetrics.apps'],
 )
