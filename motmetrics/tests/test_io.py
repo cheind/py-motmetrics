e04b90f33f81ffda8c9c13d85fcd46866479a55f
Christoph Heindl <christoph.heindl@gmail.com>
2017-04-13 07:33:14 +0200

    Adding test for motchallenge data loading

--- motmetrics/tests/test_io.py
+++ motmetrics/tests/test_io.py
@@ -13,3 +13,16 @@ def test_load_vatic():
     ])
 
     assert (df.reset_index().values == expected.values).all()
+
+def test_load_motchallenge():
+    df = mm.io.loadtxt('etc/data/iotest/motchallenge.txt', fmt=mm.io.Format.MOT15_2D)
+
+    expected = pd.DataFrame([
+        (1,1,398,181,121,229,1,-1,-1), #Note -1 on x and y for correcting matlab
+        (1,2,281,200,92,184,1,-1,-1),
+        (2,2,268,201,87,182,1,-1,-1),
+        (2,3,70,150,100,284,1,-1,-1),
+        (2,4,199,205,55,137,1,-1,-1),
+    ])
+
+    assert (df.reset_index().values == expected.values).all()
