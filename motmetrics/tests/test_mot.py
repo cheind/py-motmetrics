608026ccece846fad793bc9705098e7f7df71893
Christoph Heindl <christoph.heindl@gmail.com>
2018-02-04 09:31:45 +0100

    Adapted to raw events

--- motmetrics/tests/test_mot.py
+++ motmetrics/tests/test_mot.py
@@ -26,30 +26,42 @@ def test_events():
     expect.loc[(0, 1), :] = ['FP', np.nan, 'b', np.nan]
     expect.loc[(1, 0), :] = ['MISS', 1, np.nan, np.nan]
     expect.loc[(1, 1), :] = ['MISS', 2, np.nan, np.nan]
-    expect.loc[(2, 0), :] = ['MATCH', 1, 'b', 0.5]
-    expect.loc[(2, 1), :] = ['MATCH', 2, 'a', 0.3]
-    expect.loc[(3, 0), :] = ['SWITCH', 1, 'a', 0.2]
-    expect.loc[(3, 1), :] = ['SWITCH', 2, 'b', 0.1]
-    expect.loc[(4, 0), :] = ['MATCH', 1, 'a', 5.]
-    expect.loc[(4, 1), :] = ['MATCH', 2, 'b', 5.]
+
+    expect.loc[(2, 0), :] = ['RAW', 1, 'a', 1.0]
+    expect.loc[(2, 1), :] = ['RAW', 1, 'b', 0.5]
+    expect.loc[(2, 2), :] = ['RAW', 2, 'a', 0.3]
+    expect.loc[(2, 3), :] = ['RAW', 2, 'b', 1.0]
+    expect.loc[(2, 4), :] = ['MATCH', 1, 'b', 0.5]
+    expect.loc[(2, 5), :] = ['MATCH', 2, 'a', 0.3]
+
+    expect.loc[(3, 0), :] = ['RAW', 1, 'a', 0.2]
+    expect.loc[(3, 1), :] = ['RAW', 2, 'b', 0.1]
+    expect.loc[(3, 2), :] = ['SWITCH', 1, 'a', 0.2]
+    expect.loc[(3, 3), :] = ['SWITCH', 2, 'b', 0.1]
+
+    expect.loc[(4, 0), :] = ['RAW', 1, 'a', 5.]
+    expect.loc[(4, 1), :] = ['RAW', 1, 'b', 1.]
+    expect.loc[(4, 2), :] = ['RAW', 2, 'a', 1.]
+    expect.loc[(4, 3), :] = ['RAW', 2, 'b', 5.]
+    expect.loc[(4, 4), :] = ['MATCH', 1, 'a', 5.]
+    expect.loc[(4, 5), :] = ['MATCH', 2, 'b', 5.]
     # frame 5 generates no events
 
     assert pd.DataFrame.equals(acc.events, expect)
     
-    mh = mm.metrics.create()
-    metr = mh.compute(acc, metrics=['motp', 'mota'], return_dataframe=False)
-    assert metr['motp'] == approx(11.1 / 6)
-    assert metr['mota'] == approx(1. - (2 + 2 + 2) / 8)
 
 def test_max_switch_time():
      acc = mm.MOTAccumulator(max_switch_time=1)
      df = acc.update([1, 2], ['a', 'b'], [[1, 0.5], [0.3, 1]], frameid=1) # 1->a, 2->b
      df = acc.update([1, 2], ['a', 'b'], [[0.5, np.nan], [np.nan, 0.5]], frameid=2) # 1->b, 2->a 
+
+     df = df[df.Type != 'RAW']
      assert (df.Type == 'SWITCH').all()
 
      acc = mm.MOTAccumulator(max_switch_time=1)
      df = acc.update([1, 2], ['a', 'b'], [[1, 0.5], [0.3, 1]], frameid=1) # 1->a, 2->b
      df = acc.update([1, 2], ['a', 'b'], [[0.5, np.nan], [np.nan, 0.5]], frameid=5) # Later frame 1->b, 2->a 
+     df = df[df.Type != 'RAW']
      assert (df.Type == 'MATCH').all()
 
 def test_auto_id():
@@ -66,23 +78,3 @@ def test_auto_id():
     acc = mm.MOTAccumulator(auto_id=False)
     with pytest.raises(AssertionError):
         acc.update([1, 2, 3, 4], [], [])
-
-def test_correct_average():
-    # Tests what is being depicted in figure 3 of 'Evaluating MOT Performance'
-    acc = mm.MOTAccumulator(auto_id=True)
-
-    # No track
-    acc.update([1, 2, 3, 4], [], [])
-    acc.update([1, 2, 3, 4], [], [])
-    acc.update([1, 2, 3, 4], [], [])
-    acc.update([1, 2, 3, 4], [], [])
-
-    # Track single
-    acc.update([4], [4], [0])
-    acc.update([4], [4], [0])
-    acc.update([4], [4], [0])
-    acc.update([4], [4], [0])
-
-    mh = mm.metrics.create()
-    metr = mh.compute(acc, metrics='mota', return_dataframe=False)
-    assert metr['mota'] == approx(0.2)
