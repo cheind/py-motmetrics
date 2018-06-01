d37d9c2111c08195bd3dfaaf02f05278fb722a5e
fengweitao <fengweitao@sensetime.com>
2018-06-01 21:52:39 +0800

    more like official ver

---
+++ motmetrics/apps/evaluateTracking.py
@@ -0,0 +1,125 @@
+"""py-motmetrics - metrics for multiple object tracker (MOT) benchmarking with RESULT PREPROCESS.
+
+TOKA, 2018
+ORIGIN: https://github.com/cheind/py-motmetrics
+EXTENDED: <reposity>
+"""
+
+import argparse
+import glob
+import os
+import logging
+import motmetrics as mm
+import pandas as pd
+from collections import OrderedDict
+from pathlib import Path
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="""
+Compute metrics for trackers using MOTChallenge ground-truth data with data preprocess.
+
+Files
+-----
+All file content, ground truth and test files, have to comply with the
+format described in 
+
+Milan, Anton, et al. 
+"Mot16: A benchmark for multi-object tracking." 
+arXiv preprint arXiv:1603.00831 (2016).
+https://motchallenge.net/
+
+Structure
+---------
+
+Layout for ground truth data
+    <GT_ROOT>/<SEQUENCE_1>/gt/gt.txt
+    <GT_ROOT>/<SEQUENCE_2>/gt/gt.txt
+    ...
+
+Layout for test data
+    <TEST_ROOT>/<SEQUENCE_1>.txt
+    <TEST_ROOT>/<SEQUENCE_2>.txt
+    ...
+
+Seqmap for test data
+    [name]
+    <SEQUENCE_1>
+    <SEQUENCE_2>
+    ...
+
+Sequences of ground truth and test will be matched according to the `<SEQUENCE_X>`
+string in the seqmap.""", formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('groundtruths', type=str, help='Directory containing ground truth files.')   
+    parser.add_argument('tests', type=str, help='Directory containing tracker result files')
+    parser.add_argument('seqmap', type=str, help='Text file containing all sequences name')
+    parser.add_argument('--loglevel', type=str, help='Log level', default='info')
+    parser.add_argument('--fmt', type=str, help='Data format', default='mot15-2D')
+    parser.add_argument('--solver', type=str, help='LAP solver to use')
+    return parser.parse_args()
+
+def compare_dataframes(gts, ts):
+    accs = []
+    names = []
+    for k, tsacc in ts.items():
+        if k in gts:            
+            logging.info('Evaluating {}...'.format(k))
+            accs.append(mm.utils.CLEAR_MOT_M(gts[k], tsacc, 'iou', distth=0.5))
+            names.append(k)
+        else:
+            logging.warning('No ground truth for {}, skipping.'.format(k))
+
+    return accs, names
+
+def parseSequences(seqmap):
+    assert os.path.isfile(seqmap), 'Seqmap %s not found.'%seqmap
+    fd = open(seqmap)
+    res = []
+    for row in fd.readlines():
+        row = row.strip()
+        if row=='' or row=='name' or row[0]=='#': continue
+        res.append(row)
+    fd.close()
+    return res
+
+if __name__ == '__main__':
+
+    args = parse_args()
+
+    loglevel = getattr(logging, args.loglevel.upper(), None)
+    if not isinstance(loglevel, int):
+        raise ValueError('Invalid log level: {} '.format(args.loglevel))        
+    logging.basicConfig(level=loglevel, format='%(asctime)s %(levelname)s - %(message)s', datefmt='%I:%M:%S')
+
+    if args.solver:
+        mm.lap.default_solver = args.solver
+
+    seqs = parseSequences(args.seqmap)
+    gtfiles = [os.path.join(args.groundtruths, i, 'gt/gt.txt') for i in seqs]
+    tsfiles = [os.path.join(args.tests, '%s.txt'%i) for i in seqs]
+
+    for gtfile in gtfiles:
+        if not os.path.isfile(gtfile):
+            logging.error('gt File %s not found.'%gtfile)
+            exit(1)
+    for tsfile in tsfiles:
+        if not os.path.isfile(tsfile):
+            logging.error('res File %s not found.'%tsfile)
+            exit(1)
+
+    logging.info('Found {} groundtruths and {} test files.'.format(len(gtfiles), len(tsfiles)))
+    logging.info('Available LAP solvers {}'.format(mm.lap.available_solvers))
+    logging.info('Default LAP solver \'{}\''.format(mm.lap.default_solver))
+    logging.info('Loading files.')
+    
+    gt = OrderedDict([(seqs[i], mm.io.loadtxt(f, fmt=args.fmt, min_confidence=1)) for i, f in enumerate(gtfiles)])
+    ts = OrderedDict([(seqs[i], mm.io.loadtxt(f, fmt=args.fmt)) for i, f in enumerate(tsfiles)])    
+
+    mh = mm.metrics.create()    
+    accs, names = compare_dataframes(gt, ts)
+    
+    logging.info('Running metrics')
+    
+    summary = mh.compute_many(accs, names=names, metrics=mm.metrics.motchallenge_metrics, generate_overall=True)
+    print(mm.io.render_summary(summary, formatters=mh.formatters, namemap=mm.io.motchallenge_metric_names))
+    logging.info('Completed')
--- motmetrics/utils.py
+++ motmetrics/utils.py
@@ -2,6 +2,7 @@
 
 Christoph Heindl, 2017
 https://github.com/cheind/py-motmetrics
+TOKA EXTENDED THIS FILE.
 """
 
 import pandas as pd
@@ -11,6 +12,67 @@ from .mot import MOTAccumulator
 from .distances import iou_matrix, norm2squared_matrix
 
 def compare_to_groundtruth(gt, dt, dist='iou', distfields=['X', 'Y', 'Width', 'Height'], distth=0.5):
+    """Compare groundtruth and detector results.
+
+    This method assumes both results are given in terms of DataFrames with at least the following fields
+     - `FrameId` First level index used for matching ground-truth and test frames.
+     - `Id` Secondary level index marking available object / hypothesis ids
+    
+    Depending on the distance to be used relevant distfields need to be specified.
+
+    Params
+    ------
+    gt : pd.DataFrame
+        Dataframe for ground-truth
+    test : pd.DataFrame
+        Dataframe for detector results
+    
+    Kwargs
+    ------
+    dist : str, optional
+        String identifying distance to be used. Defaults to intersection over union.
+    distfields: array, optional
+        Fields relevant for extracting distance information. Defaults to ['X', 'Y', 'Width', 'Height']
+    distth: float, optional
+        Maximum tolerable distance. Pairs exceeding this threshold are marked 'do-not-pair'.
+    """
+
+    def compute_iou(a, b):
+        return iou_matrix(a, b, max_iou=distth)
+
+    def compute_euc(a, b):
+        return norm2squared_matrix(a, b, max_d2=distth)
+
+    compute_dist = compute_iou if dist.upper() == 'IOU' else compute_euc
+
+    acc = MOTAccumulator()
+
+    # We need to account for all frames reported either by ground truth or
+    # detector. In case a frame is missing in GT this will lead to FPs, in 
+    # case a frame is missing in detector results this will lead to FNs.
+    allframeids = gt.index.union(dt.index).levels[0]
+    
+    for fid in allframeids:
+        oids = np.empty(0)
+        hids = np.empty(0)
+        dists = np.empty((0,0))
+
+        if fid in gt.index:
+            fgt = gt.loc[fid] 
+            oids = fgt.index.values
+
+        if fid in dt.index:
+            fdt = dt.loc[fid]
+            hids = fdt.index.values
+
+        if oids.shape[0] > 0 and hids.shape[0] > 0:
+            dists = compute_dist(fgt[distfields].values, fdt[distfields].values)
+        
+        acc.update(oids, hids, dists, frameid=fid)
+    
+    return acc
+
+def CLEAR_MOT_M(gt, dt, dist='iou', distfields=['X', 'Y', 'Width', 'Height'], distth=0.5):
     """Compare groundtruth and detector results.
 
     This method assumes both results are given in terms of DataFrames with at least the following fields
