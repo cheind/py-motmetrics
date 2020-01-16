3d8be1ec65c00df8e0a4d310292e92df47a24018
Jack Valmadre <valmadre@google.com>
2020-01-16 16:27:46 +0100

    Use numpy broadcasting to compute IOU

--- motmetrics/distances.py
+++ motmetrics/distances.py
@@ -49,28 +49,44 @@ def norm2squared_matrix(objs, hyps, max_d2=float('inf')):
     C[C > max_d2] = np.nan
     return C
 
+
+def rect_min_max(r):
+    min_pt = r[..., :2]
+    size = r[..., 2:]
+    max_pt = min_pt + size
+    return min_pt, max_pt
+
+
 def boxiou(a, b):
-    rx1 = max(a[0], b[0])
-    rx2 = min(a[0]+a[2], b[0]+b[2])
-    ry1 = max(a[1], b[1])
-    ry2 = min(a[1]+a[3], b[1]+b[3])
-    if ry2>ry1 and rx2>rx1:
-        i = (ry2-ry1)*(rx2-rx1)
-        u = a[2]*a[3]+b[2]*b[3]-i
-        return float(i)/u
-    else: return 0.0
+    a_min, a_max = rect_min_max(a)
+    b_min, b_max = rect_min_max(b)
+    # Compute intersection.
+    i_min = np.maximum(a_min, b_min)
+    i_max = np.minimum(a_max, b_max)
+    i_size = np.maximum(i_max - i_min, 0)
+    i_vol = np.prod(i_size, axis=-1)
+    # Get volume of union.
+    a_size = np.maximum(a_max - a_min, 0)
+    b_size = np.maximum(b_max - b_min, 0)
+    a_vol = np.prod(a_size, axis=-1)
+    b_vol = np.prod(b_size, axis=-1)
+    u_vol = a_vol + b_vol - i_vol
+    # return np.where(i_vol == 0, np.zeros_like(i_vol), i_vol / u_vol)
+    # TODO: Use quiet divide here to avoid warning?
+    return i_vol / u_vol
+
 
 def iou_matrix(objs, hyps, max_iou=1.):
     """Computes 'intersection over union (IoU)' distance matrix between object and hypothesis rectangles.
 
-    The IoU is computed as 
-        
+    The IoU is computed as
+
         IoU(a,b) = 1. - isect(a, b) / union(a, b)
 
     where isect(a,b) is the area of intersection of two rectangles and union(a, b) the area of union. The
     IoU is bounded between zero and one. 0 when the rectangles overlap perfectly and 1 when the overlap is
     zero.
-    
+
     Params
     ------
     objs : Nx4 array
@@ -91,43 +107,13 @@ def iou_matrix(objs, hyps, max_iou=1.):
         Distance matrix containing pairwise distances or np.nan.
     """
 
-    #import time
-    #st = time.time()
-    objs = np.atleast_2d(objs).astype(float)
-    hyps = np.atleast_2d(hyps).astype(float)
-    if objs.size == 0 or hyps.size == 0:
-        return np.empty((0,0))
+    if np.size(objs) == 0 or np.size(hyps) == 0:
+        return np.empty((0, 0))
 
+    objs = np.asfarray(objs)
+    hyps = np.asfarray(hyps)
     assert objs.shape[1] == 4
     assert hyps.shape[1] == 4
-
-    br_objs = objs[:, :2] + objs[:, 2:]
-    br_hyps = hyps[:, :2] + hyps[:, 2:]
-
-    C = np.empty((objs.shape[0], hyps.shape[0]))
-
-    for o in range(objs.shape[0]):
-        for h in range(hyps.shape[0]):
-            #isect_xy = np.maximum(objs[o, :2], hyps[h, :2])
-            #isect_wh = np.maximum(np.minimum(br_objs[o], br_hyps[h]) - isect_xy, 0)
-            #isect_a = isect_wh[0]*isect_wh[1]
-            #union_a = objs[o, 2]*objs[o, 3] + hyps[h, 2]*hyps[h, 3] - isect_a
-            #if union_a != 0:
-            #    C[o, h] = 1. - isect_a / union_a
-            #else:
-            #    C[o, h] = np.nan
-            iou = boxiou(objs[o], hyps[h])
-            if 1 - iou > max_iou:
-                C[o, h] = np.nan
-            else:
-                C[o, h] = 1 - iou
-
-    #C[C > max_iou] = np.nan
-    #print('----'*2,'done',time.time()-st)
-    return C
-    
-
-
-
-
-
+    iou = boxiou(objs[:, None], hyps[None, :])
+    dist = 1 - iou
+    return np.where(dist > max_iou, np.nan, dist)
