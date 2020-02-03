16b9262c460fc5b97f7192f1e5c9d940e0d3f79e
Jack Valmadre <valmadre@google.com>
2020-02-03 09:03:33 +0100

    Exclude lapsolver from tests in python 2

--- motmetrics/tests/test_lap.py
+++ motmetrics/tests/test_lap.py
@@ -4,12 +4,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
+
 import numpy as np
 import pytest
 
 from motmetrics import lap
 
 SOLVERS = ['scipy', 'munkres', 'ortools', 'lap', 'lapsolver']
+if sys.version_info >= (3,):
+    SOLVERS = [x for x in SOLVERS if x not in ['lapsolver']]
 
 
 @pytest.mark.parametrize('solver', SOLVERS)
