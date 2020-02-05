655420b109df609889419255edf3233c1bd48b30
Jack Valmadre <valmadre@google.com>
2020-02-05 15:40:53 +0100

    Revert setup.py while debugging TestPyPI

--- setup.py
+++ setup.py
@@ -4,26 +4,19 @@ Christoph Heindl, 2017
 https://github.com/cheind/py-motmetrics
 """
 
-import io
-
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-with io.open('requirements.txt') as f:
+with open('requirements.txt') as f:
     required = f.read().splitlines()
-
-with io.open('Readme.md', encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='motmetrics',
-    version=io.open('motmetrics/__init__.py').readlines()[-1].split()[-1].strip('\''),
+    version=open('motmetrics/__init__.py').readlines()[-1].split()[-1].strip('\''),
     description='Metrics for multiple object tracker benchmarking.',
     author='Christoph Heindl, Jack Valmadre',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     url='https://github.com/cheind/py-motmetrics',
     license='MIT',
     install_requires=required,
