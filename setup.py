4b06af4378a8e411657f856a01d9104bff616eb6
Jack Valmadre <valmadre@google.com>
2020-02-05 14:03:36 +0100

    Read long_description from Readme.md

--- setup.py
+++ setup.py
@@ -12,11 +12,16 @@ except ImportError:
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+with open('Readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='motmetrics',
     version=open('motmetrics/__init__.py').readlines()[-1].split()[-1].strip('\''),
     description='Metrics for multiple object tracker benchmarking.',
     author='Christoph Heindl, Jack Valmadre',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/cheind/py-motmetrics',
     license='MIT',
     install_requires=required,
