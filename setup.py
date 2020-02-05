417c124b354e9e7d2d9c13ceee42e2c34220a9f3
Jack Valmadre <valmadre@google.com>
2020-02-05 14:10:46 +0100

    Use io.open() for compatibility

--- setup.py
+++ setup.py
@@ -4,20 +4,22 @@ Christoph Heindl, 2017
 https://github.com/cheind/py-motmetrics
 """
 
+import io
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-with open('requirements.txt') as f:
+with io.open('requirements.txt') as f:
     required = f.read().splitlines()
 
-with open('Readme.md', encoding='utf-8') as f:
+with io.open('Readme.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='motmetrics',
-    version=open('motmetrics/__init__.py').readlines()[-1].split()[-1].strip('\''),
+    version=io.open('motmetrics/__init__.py').readlines()[-1].split()[-1].strip('\''),
     description='Metrics for multiple object tracker benchmarking.',
     author='Christoph Heindl, Jack Valmadre',
     long_description=long_description,
