f9f0c86c3c0556a4b45430b3500f4cd342a9e7e5
Jack Valmadre <valmadre@google.com>
2020-09-20 22:32:43 +0200

    Remove predictions that match to ignored classes

--- motmetrics/tests/test_issue126.py
+++ motmetrics/tests/test_issue126.py
@@ -19,6 +19,7 @@ import glob
 import os
 import pathlib
 
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -27,6 +28,14 @@ import motmetrics as mm
 DATA_DIR = os.path.join(os.path.dirname(__file__), '../data')
 SOLVERS = ['scipy', 'lap', 'lapsolver', 'ortools']
 
+# https://github.com/dendorferpatrick/MOTChallengeEvalKit/blob/56aa36785a145184037412d45ed04b0c74c0ff89/matlab_devkit/utils/preprocessResult.m#L72
+MOT_IGNORE_CATEGORIES = [
+        2,  # person_on_vhcl
+        7,  # static_person
+        8,  # distractor
+        12,  # reflection
+        # 6,  # non_mot_vhcl (MOT20 only?)
+]
 
 @pytest.mark.parametrize('solver', SOLVERS)
 def test_issue126(solver):
@@ -47,6 +56,46 @@ def test_issue126(solver):
 
     gt = [mm.io.loadtxt(f, fmt='mot16') for f in gt_files]
     pred = [mm.io.loadtxt(f, fmt='mot16') for f in pred_files]
+    pred_subset = [None for _ in pred_files]
+
+    # Check whether there are some matches to other classes.
+    num_gt = collections.Counter()
+    num_pred = collections.Counter()
+    FIELDS = ['X', 'Y', 'Width', 'Height']
+    for i in range(len(seqs)):
+        num_frames = gt[i].index.get_level_values('FrameId').max()
+        frame_to_gt = dict(iter(gt[i].groupby('FrameId')))
+        frame_to_pred = dict(iter(pred[i].groupby('FrameId')))
+        frame_to_pred_subset = {}
+        for t in range(1, num_frames + 1):
+            if t in frame_to_gt:
+                num_gt.update(frame_to_gt[t]['ClassId'])
+            if t in frame_to_pred:
+                # Assume all predictions add to class 1.
+                num_pred[1] += len(frame_to_pred[t])
+
+            if not (t in frame_to_gt and t in frame_to_pred):
+                continue
+            dists = mm.distances.iou_matrix(frame_to_gt[t][FIELDS].values, frame_to_pred[t][FIELDS].values, 0.5)
+            is_match = ~np.isnan(dists)
+            # is_pedestrian = np.asarray(frame_to_gt[t]['ClassId'] == 1)[:, np.newaxis]
+            # weights = np.where(is_match, 1e3 * is_pedestrian + is_match + 1e-3 * (1 - dists), 0)
+            weights = np.where(is_match, is_match + 1e-3 * (1 - dists), 0)
+            gt_ind, pred_ind = mm.lap.linear_sum_assignment(-weights)
+            non_zero_subset = is_match[gt_ind, pred_ind]
+            gt_ind = gt_ind[non_zero_subset]
+            pred_ind = pred_ind[non_zero_subset]
+            gt_cat = frame_to_gt[t].iloc[gt_ind]['ClassId']
+
+            # Find predictions to exclude.
+            exclude_ind = pred_ind[gt_cat.isin(MOT_IGNORE_CATEGORIES)]
+            exclude_mask = _subset_to_mask(len(frame_to_pred[t]), exclude_ind)
+            frame_to_pred_subset[t] = frame_to_pred[t][~exclude_mask]
+
+        # Note: Assumes that the predictions are not empty.
+        pred_subset[i] = pd.concat([frame_to_pred_subset[t] for t in sorted(frame_to_pred_subset)])
+
+    pred = pred_subset
 
     # Take only pedestrian class.
     gt = [df[df['ClassId'] == 1] for df in gt]
@@ -69,3 +118,9 @@ def test_issue126(solver):
 
         overall = summary.loc['OVERALL']
         pd.testing.assert_series_equal(overall, expected, check_names=False)
+
+
+def _subset_to_mask(n, subset):
+    mask = np.zeros(n, dtype=np.bool)
+    mask[subset] = True
+    return mask
